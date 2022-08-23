634021364682c25f1bb0c2cc48dded0b2aa6977f
OmarHamdann <omar.haamdan@gmail.com>
2022-08-23 11:57:10 +0300

    Parameters Binding

--- webAPI/Controllers/BooksController.cs
+++ webAPI/Controllers/BooksController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using webAPI.Models;
 
 namespace webAPI.Controllers
 {
@@ -15,10 +16,22 @@ namespace webAPI.Controllers
 
             return string.Format("Title ={0}, Number of pages ={1}", title, pages);
         }
-        
 
-       
 
-      
+        public string PostBook(string title, string pages)
+        {
+
+            return string.Format("Title ={0}, Number of pages ={1}", title, pages);
+        }
+
+
+        //using model (complex type)
+        public string GetBook(book book)
+        {
+
+            return string.Format("Title ={0}, Number of pages ={1}", book.Title ,book.Pages);
+        }
+
+
     }
 }
